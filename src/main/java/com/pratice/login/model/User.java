de7b033e06d77f8c2d4e3bc944d18d682dd742d6
DivyaS <127476291+Sdivya15@users.noreply.github.com>
2026-03-03 06:54:37 +0530

    Added user model for Login page

---
+++ src/main/java/com/pratice/login/model/User.java
@@ -0,0 +1,20 @@
+package com.pratice.login.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+}
